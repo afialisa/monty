56028bb32e9076c669a1d137574f9a6c09b1941c
afialisa <afianourish72@gmail.com>
2024-01-18 20:55:04 -0800

    monty repo

--- main.c
+++ main.c
@@ -1,61 +1,46 @@
 #include "monty.h"
-
-void execute(FILE *file);
-
+bus_t bus = {NULL, NULL, NULL, 0};
 /**
- * main - Monty code interpreter
- * @argc: Number of arguments
- * @argv: Monty file location
- * Return: 0 (success)
- */
+* main - monty code interpreter
+* @argc: number of arguments
+* @argv: monty file location
+* Return: 0 on success
+*/
 int main(int argc, char *argv[])
 {
+	char *content;
 	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
 	if (argc != 2)
 	{
 		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
-
 	file = fopen(argv[1], "r");
+	bus.file = file;
 	if (!file)
 	{
 		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
-
-	execute(file);
-
-	fclose(file);
-	return (0);
-}
-
-/**
- * execute - A function that executes monty commands
- * @file: file pointer
- */
-void execute(FILE *file)
-{
-	size_t size = 0;
-	ssize_t read_line = 1;
-	char *content = NULL;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
-
 	while (read_line > 0)
 	{
 		content = NULL;
 		read_line = getline(&content, &size, file);
+		bus.content = content;
 		counter++;
-
 		if (read_line > 0)
 		{
-			/* Implement logic using content, stack, counter, and file */
+			execute(content, &stack, counter, file);
 		}
-
 		free(content);
 	}
-
 	free_stack(stack);
+	fclose(file);
+return (0);
 }
+
